76413ea19d08a179105b3603e83f314f99d40176
yangzd0814 <yangzd0814@linux03.cs.nctu.edu.tw>
2018-09-03 21:20:22 +0800

    Ge code

--- Geometry/ClosestPair.cpp
+++ Geometry/ClosestPair.cpp
@@ -1,20 +1,48 @@
-#include "Point.cpp"
+#include<cmath>
+#include<vector>
+#include<algorithm>
 using namespace std;
-const double INF = 0x3f3f3f3f;
-bool cmpx(P a, P b) { return a.x < b.x; }
-bool cmpy(P a, P b) { return a.y < b.y; }
-pair<P, P> DnC(vector<P> &p, int L, int R) {
-    if (R - L <= 1) return make_pair(P(-INF, -INF), P(INF, INF));
-    int M = (L + R) >> 1;
-    pair<P, P> l = DnC(p, L, M);
-    pair<P, P> r = DnC(p, M, R);
-    
-
+template<typename T>
+struct point{
+	T x,y;
+	point(){}
+	point(const T&dx,const T&dy):x(dx),y(dy){}
+	inline const point operator-(const point &b)const{
+		return point(x-b.x,y-b.y);
+	}
+	inline const T dot(const point &b)const{
+		return x*b.x+y*b.y;
+	}
+	inline const T abs2()const{/*向量長度的平方*/
+		return dot(*this);
+	}
+	static bool x_cmp(const point<T>& a,const point<T>& b){
+		return a.x<b.x;
+	}
+	static bool y_cmp(const point<T>& a,const point<T>& b){
+		return a.y<b.y;
+	}
+};
+#define INF LLONG_MAX/*預設是long long最大值*/
+template<typename T>
+T closest_pair(vector<point<T> >&v,vector<point<T> >&t,int l,int r){
+	T dis=INF,tmd;
+	if(l>=r)return dis;
+	int mid=(l+r)/2;
+	if((tmd=closest_pair(v,t,l,mid))<dis)dis=tmd;
+	if((tmd=closest_pair(v,t,mid+1,r))<dis)dis=tmd;
+	t.clear();
+	for(int i=l;i<=r;++i)
+		if((v[i].x-v[mid].x)*(v[i].x-v[mid].x)<dis)t.push_back(v[i]);
+	sort(t.begin(),t.end(),point<T>::y_cmp);/*如果用merge_sort的方式可以O(n)*/
+	for(int i=0;i<(int)t.size();++i)
+		for(int j=1;j<=3&&i+j<(int)t.size();++j)
+			if((tmd=(t[i]-t[i+j]).abs2())<dis)dis=tmd;
+	return dis;
 }
-pair<P, P> closestPair(vector<P> &p) {
-    sort(p.begin(), p.end(), cmpx);
-    return DnC(p, 0, p.size());
-}
-int main() {
-
+template<typename T>
+inline T closest_pair(vector<point<T> > &v){
+	vector<point<T> >t;
+	sort(v.begin(),v.end(),point<T>::x_cmp);
+	return closest_pair(v,t,0,v.size()-1);/*最近點對距離*/
 }
--- Geometry/RotatingCaliper.cpp
+++ Geometry/RotatingCaliper.cpp
@@ -1,6 +1,56 @@
-#include "Point.cpp"
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-
+const int MAXN = 1e5 + 5;
+struct Point {
+    int x, y;
+    Point operator - (const Point &rhs) const {
+        Point ret;
+        ret.x = x - rhs.x;
+        ret.y = y - rhs.y;
+        return ret;
+    }
+} P[MAXN];
+int n;
+typedef Point Vector;
+int cross(Vector v1, Vector v2) {
+    return v1.x * v2.y - v1.y * v2.x;
 }
+int cross(Point o, Point a, Point b) {
+    return (a.x-o.x) * (b.y-o.y) - (a.y-o.y) * (b.x-o.x);
+}
+Point L[MAXN+1], U[MAXN];
+bool cmp(Point a, Point b) {
+    return (a.x < b.x) || (a.x == b.x && a.y < b.y);
+}
+int rotating_caliper() {
+    sort(P, P+n, cmp);
+    int l = 0, u = 0;
+    for (int i = 0 ; i < n ; i++)
+    {
+        while (l >= 2 && cross(L[l-2], L[l-1], P[i]) <= 0) l--;
+        while (u >= 2 && cross(U[u-2], U[u-1], P[i]) >= 0) u--;
+        L[l++] = P[i];
+        U[u++] = P[i];
+    }
+    if (u-2 >= 0) L[l] = U[u-2];
+    int max_dist = 0;
+    for (int i = 0, j = u-1 ; i < l && j > 0 ; )
+    {
+        max_dist = max(max_dist, (L[i].x - U[j].x) * (L[i].x - U[j].x) + (L[i].y - U[j].y) * (L[i].y - U[j].y));
+        if (cross(L[i+1] - L[i], U[j-1] - U[j]) < 0)
+            i++;
+        else
+            j--;
+    }
+    return max_dist;
+}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int t; cin >> t; while (t--) {
+        cin >> n; for (int i = 0 ; i < n ; i++) {
+            cin >> P[i].x >> P[i].y;
+        }
+        cout << rotating_caliper() << '\n';
+    }
+}
+
