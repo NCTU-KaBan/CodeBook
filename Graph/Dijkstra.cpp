065952b7fed248995ca620d4be602a70b5d48867
Marmot Yang <s1357andy871207@gmail.com>
2018-11-03 16:19:27 +0800

    dij

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,33 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 0x3f3f3f3f;
-typedef pair<int, int> pii;
-vector<int> Dijkstra(vector<vector<pii> > &G, int s, int t) {
-    vector<int> d(G.size(), INF);
-    priority_queue<pii> pq;
-    d[s] = 0; pq.push({-d[s], s});
+typedef long long LL;
+struct Edge {
+    int v; LL w;
+    bool operator < (const Edge &e) const {
+        return w > e.w;
+    }
+};
+const LL INF = 0x3f3f3f3f3f3f3f3fLL;
+vector<LL> Dijkstra(vector<vector<Edge> > &G, int s) {
+    vector<LL> d(G.size(), INF);
+    priority_queue<Edge> pq;
+    d[s] = 0; pq.push({s, d[s]});
     while (pq.size()) {
-        pii p = pq.top(); pq.pop();
-        int u = p.second;
-        if (d[u] < -p.first) continue;
-        for (auto e : G[u]) {
-            if (d[e.first] < d[u] + e.second) continue;
-            d[e.first] = d[u] + e.second;
-            pq.push({-d[e.first], e.first});
+        Edge p = pq.top(); pq.pop();
+        if (d[p.v] < p.w) continue;
+        for (auto e : G[p.v]) {
+            if (d[e.v] < d[p.v] + e.w) continue;
+            d[e.v] = d[p.v] + e.w;
+            pq.push({e.v, d[e.v]});
         }
     }
     return d;
 }
 int main() {
-    int n, m; cin >> n >> m;
-    vector<vector<pii> > G(n);
-    for (int i = 0 ; i < m ; i++) {
-        int u, v, w; cin >> u >> v >> w;
-        G[u].push_back({v, w});
-        G[v].push_back({u, w});
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int n, m; while (cin >> n >> m) {
+        vector<vector<Edge> > G(n);
+        while (m--) {
+            int u, v; LL w; cin >> u >> v >> w;
+            G[u].push_back({v, w});
+            G[v].push_back({u, w});
+        }
+        int s, t; cin >> s >> t;
+        vector<LL> d = Dijkstra(G, s);
+        cout << (d[t] != INF ? d[t] : -1) << '\n';
     }
-    vector<int> d = Dijkstra(G, 0, n - 1);
-    for (int i = 0 ; i < n ; i++)
-        cout << d[i] << ' ';
-    cout << '\n';
 }
