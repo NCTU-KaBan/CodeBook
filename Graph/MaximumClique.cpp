959b73bbe374b9a2ecf7d714048ef4bf4035429c
Marmot Yang <s1357andy871207@gmail.com>
2018-12-10 17:36:43 +0800

    add something

--- Graph/MaximumClique.cpp
+++ Graph/MaximumClique.cpp
@@ -1,62 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int MAXN = 35;
-bool G[MAXN][MAXN];
-struct Set { 
-    bool s[MAXN]; int size; 
-    Set() { memset(s, false, sizeof(s)); size = 0; }
-};
-int n, m, maximum_clique;
-Set intersect(Set S, int u) {
-    for (int i = 0 ; i < n ; i++) {
-        if (S.s[i] && !G[u][i]) {
-            S.s[i] = false;
-            S.size--;
+t int MAXN = 105;
+int best;
+int n;
+int num[MAXN];
+int path[MAXN];
+int G[MAXN][MAXN];
+
+bool dfs( int *adj, int total, int cnt ){
+    int t[MAXN];
+    if (total == 0){
+        if( best < cnt ){
+            best = cnt;
+            return true;
         }
+        return false;
     }
-    return S;
+    for(int i = 0; i < total; i++){
+        if( cnt+(total-i) <= best ) return false;
+        if( cnt+num[adj[i]] <= best ) return false;
+        int k=0;
+        for(int j=i+1; j<total; j++)
+            if(G[ adj[i] ][ adj[j] ])
+                t[k++] = adj[j];
+        if (dfs(t, k, cnt+1)) return true;
+    }
+    return false;
 }
-void backtrack(Set R, Set P, Set X) {
-    if (P.size == 0) {
-        if (X.size == 0) {
-            maximum_clique = max(maximum_clique, R.size);
-        }
-        return ;
+int MaximumClique(){
+    int adj[MAXN];
+    if (n <= 0) return 0;
+    best = 0;
+    for(int i = n-1; i >= 0; i--){
+        int k=0;
+        for(int j = i+1; j < n; j++)
+            if (g[i][j]) adj[k++] = j;
+        dfs( adj, k, 1 );
+        num[i] = best;
     }
-    int pivot;
-    for (pivot = 0 ; pivot < n ; pivot++) 
-        if (P.s[pivot] || X.s[pivot])
-            break;
-    for (int i = 0 ; i < n ; i++) {
-        if (P.s[i] && !G[pivot][i]) {
-            R.s[i] = true; R.size++;
-            backtrack(R, intersect(P, i), intersect(X, i));
-            R.s[i] = false; R.size--;
-            P.s[i] = false; P.size--;
-            if (!X.s[i]) X.s[i] = true, X.size++;
-        }
-    }
-}
-void BK() {
-    for (int i = 0 ; i < n ; i++) G[i][i] = false;
-    Set R, P, X;
-    for (int i = 0 ; i < n ; i++) R.s[i] = false;
-    for (int i = 0 ; i < n ; i++) P.s[i] = true;
-    for (int i = 0 ; i < n ; i++) X.s[i] = false;
-    R.size = 0;
-    P.size = n;
-    X.size = 0;
-    backtrack(R, P, X);
-}
-int main() {
-    while (cin >> n >> m) {
-        memset(G, false, sizeof(G));
-        maximum_clique = 0;
-        for (int i = 0 ; i < m ; i++) {
-            int u, v; cin >> u >> v;
-            G[u][v] = G[v][u] = true;
-        }
-        BK();
-        cout << maximum_clique << '\n';
-    }
+    return best;
 }
