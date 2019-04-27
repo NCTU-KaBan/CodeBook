b54b50415554dd04c41e383b2cdc55fb5ee3a01c
marmot0814 <s1357andy871207@gmail.com>
2019-04-28 01:42:07 +0800

    SegmentTree

--- DataStructure/SegmentTree.cpp
+++ DataStructure/SegmentTree.cpp
@@ -1,82 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long LL;
-const int MAXN = 1e4 + 5;
-const int lgN = __lg(MAXN) + 5;
 struct SegmentTree {
-    struct Node {
-        int L, R; 
-        Node *l, *r;
-        // data
-        // tag
-        Node (int _L = 0,int _R = 0) : L(_L), R(_R) {
-            l = r = NULL;
-        }
-    }*rt, buf[lgN << 2], *ptr;
-    LL *arr; int n;
-    SegmentTree(LL *_arr, int _n) {
-        arr = _arr, n = _n;
-        rt = build(0, n);
+    struct Node { int L, R; Node *l, *r;
+        // data, eg. v
+        // tag, eg. add
+        Node (int L = 0, int R = 0) : L(L), R(R) { l = r = NULL; }
+    }*rt, *buf, *ptr;
+    SegmentTree (vector<int> arr) { rt = build(0, arr.size(), arr);
+        buf = new Node[__lg(arr.size()) * 4 + 5];
     }
-
-    ~SegmentTree() {
-        remove(rt);
+    Node* build(int L, int R, vector<int> &arr) {
+        Node *u = new Node(L, R); int M = (L + R) >> 1;
+        if (R - L == 1) return /*basic data,*/ u;
+        return u->l = build(L, M, arr); u->r = build(M, R, arr), pull(u);
     }
-    void remove(Node *u) {
-        if (!u) return ;
-        remove(u->r);
-        remove(u->l);
-        delete u;
-    }
-    Node* build(int L, int R) {
-        Node *u = new Node(L, R);
-        if (R - L == 1) {
-            // basic data
-            return u;
-        }
-        int M = (R + L) >> 1;
-        u->l = build(L, M);
-        u->r = build(M, R);
-        return pull(u);
-    }
-    Node* pull(Node *u) {
-        return pull(u, u->l, u->r);
-    }
+    Node* pull(Node *u) { return pull(u, u->l, u->r); }
     Node* pull(Node *u, Node *l, Node *r) {
-        if (!l || !r) return l ? l : r;
-        push(l); push(r);
-        // pull function;
+        push(l); push(r); if (!l || !r) return l ? l : r;
+        // pull function
         return u;
     }
     void push(Node *u) {
         if (!u) return ;
         // push function
     }
-    Node* query(int qL, int qR) {
-        ptr = buf;
-        return query(qL, qR, rt);
-    }
-    Node* query(int qL, int qR, Node *u) {
-        if (u->R <= qL || qR <= u->L) return (Node*)NULL;
-        if (qL <= u->L && u->R <= qR) return u;
-        push(u);
+    Node* query(int qL, int qR, Node *u = NULL) {
+        if (!u) u = rt, buf = ptr;
+        if (qR <= u->L || u->R <= qL) return (Node*)NULL;
+        push(u); if (qL <= u->L && u->R <= qR) return u;
         return pull(ptr++, query(qL, qR, u->l), query(qL, qR, u->r));
     }
-    void modify(int mL, int mR) {
-        return modify(mL, mR, rt);
+    void modify(int mL, int mR, int v, Node *u = NULL) {
+        if (!u) u = rt; push(u); if (mR <= u->L || u->R <= mL) return ;
+        if (mL <= u->L && u->R <= mR) 
+            return /*basic modify,*/ void();
+        modify(mL, mR, v, u->l); modify(mL, mR, v, u->r);
+        return pull(u), void();
     }
-    void modify(int mL, int mR, Node *u) {
-        if (u->R <= mL || mR <= u->L) return ;
-        if (mL <= u->L && u->R <= mR) {
-            // modify function
-            return ;
-        }
-        push(u);
-        modify(mL, mR, u->l);
-        modify(mL, mR, u->r);
-        pull(u);
-    }
+    ~SegmentTree() { clear(rt); delete []buf; }
+    void clear(Node *u) { if (u) clear(u->l), clear(u->r), delete u; }
 };
-int main() {
-
-}
